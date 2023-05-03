24f77708796db0da034a532772b6509d124b08aa
Sergio Majé <smajefranco@gmail.com>
2023-05-02 20:31:42 -0500

    🍱 Add new favicon to the app
    The new favicon is a paw icon in SVG format

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,10 +12,11 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
     <>
       <Head>
         <meta charSet='utf-8' />
-        <meta name='description' content='Pagina adopción mascotas' />
+        <meta name='description' content='Página adopción mascotas' />
         <meta name='author' content='Johan Garcia' />
         <meta name='robots' content='noindex' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='shortcut icon' href='favicon.svg' type='image/svg+xml' />
       </Head>
 
       {getLayout(<Component {...pageProps} />)}
