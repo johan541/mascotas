3bcaf4cf390c8e148881c03aa421fde5c4bed761
Johan Garcia <js.garcia@udla.edu.co>
2023-05-04 04:53:08 +0000

    remove Next line to Form.dto

--- src/components/Form/Form.dto.ts
+++ src/components/Form/Form.dto.ts
@@ -3,7 +3,6 @@ import type { SubmitHandler } from 'react-hook-form';
 import type { FieldAttributes } from './Field';
 import { CommandAttributes } from './Commands';
 
-
 export type FormProps<T extends Record<string, object>> = {
   readonly data: FieldAttributes<T>[];
   readonly commands: CommandAttributes;
