29eb9398b8ab025a64060337f4f0bd89d011b26f
Sergio Majé <smajefranco@gmail.com>
2023-05-22 07:05:10 -0500

    🐛 Add missing db connection in auth routes

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,13 +2,16 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import NextAuth, { NextAuthOptions } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 
-import { AuthRoutes } from '@/helpers/routes';
+import { AuthRoutes, Routes } from '@/helpers/routes';
 import { userController } from '@/lib/dependencies';
 import { type UserLogin, UserSchema } from '@/schemas/user.schema';
 import { signJwtAccessToken } from '@/lib/security/jwt';
+import { dbConnect } from '@/utils/mongoose';
 
 export default async function auth(req: NextApiRequest, res: NextApiResponse) {
   // Do whatever you want here, before the request is passed down to `NextAuth`
+
+  dbConnect();
 
   const maxAge = 60 * 60 * 4; // 4 hours
 
@@ -52,6 +55,8 @@ export default async function auth(req: NextApiRequest, res: NextApiResponse) {
     pages: {
       signIn: AuthRoutes.SIGN_IN.path,
       error: AuthRoutes.SIGN_IN.path,
+      newUser: AuthRoutes.SIGN_UP.path,
+      signOut: Routes.HOME.path,
     },
   };
 
